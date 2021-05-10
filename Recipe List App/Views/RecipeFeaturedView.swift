8278cc3df20f36e66cf9a4d22ef55a0e064bdcb8
jawed <jawedsarwari57@gmail.com>
2021-05-10 17:24:04 +0430

    - Add Button to RecipeFeaturedView -

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct RecipeFeaturedView: View {
     @EnvironmentObject var model : RecipeModel
+    @State var isDetailViewShowing = false
     var body: some View {
         
         VStack(alignment: .leading, spacing:0){
@@ -24,18 +25,29 @@ struct RecipeFeaturedView: View {
                         //Only show those that should be featured.
                         if model.recipes[index].featured{
                             
-                            ZStack{
-                                Rectangle()
-                                    .foregroundColor(.white)
-                                VStack(spacing:0){
-                                    Image(model.recipes[index].image)
-                                        .resizable()
-                                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                                        .clipped()
-                                    Text(model.recipes[index].name)
-                                        .padding(5)
+                            //MARK: Recipe Button
+                            Button(action: {
+                                
+                                self.isDetailViewShowing = true
+                                
+                            }, label:{
+                                ZStack{
+                                    Rectangle()
+                                        .foregroundColor(.white)
+                                    VStack(spacing:0){
+                                        Image(model.recipes[index].image)
+                                            .resizable()
+                                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                            .clipped()
+                                        Text(model.recipes[index].name)
+                                            .padding(5)
+                                    }
                                 }
+                            })
+                            .sheet(isPresented: $isDetailViewShowing){
+                                RecipeDetailsView(recipe: model.recipes[index])
                             }
+                            .buttonStyle(PlainButtonStyle())
                             .frame(width: geo.size.width-40, height: geo.size.height-100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                             .cornerRadius(15)
                             .shadow(color:Color(.sRGB,red: 0, green: 0, blue: 0, opacity: 0.5),radius:10, x:-5, y:5)
